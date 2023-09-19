3ddaaca24d8417b14d7f3256442bdae81df3aa41
Lena Vera <elenabolonina001@gmail.com>
2023-09-19 21:14:38 +0400

    Initial commit

---
+++ src/main/java/spring/mvc/hibernate/service/UserService.java
@@ -0,0 +1,16 @@
+package spring.mvc.hibernate.service;
+
+import spring.mvc.hibernate.model.User;
+import java.util.List;
+
+public interface UserService {
+    List<User> listUsers();
+
+    void saveUser(User user);
+
+    User getById(int id);
+
+    void update(User user);
+
+    void delete(int id);
+}
